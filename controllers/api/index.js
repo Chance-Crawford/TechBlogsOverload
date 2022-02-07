77fe3c716b2afaddc3f16579a2fdf080d63be79f
Chance <chancecrawford30@gmail.com>
2022-02-06 22:13:57 -0800

    created comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
+// use all api routes under their designated paths
 router.use('/users', require('./user-routes'));
 router.use('/posts', require('./post-routes'));
+router.use('/comments', require('./comment-routes'));
 
 module.exports = router;
